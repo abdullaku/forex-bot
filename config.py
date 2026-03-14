918022b722a056693b4f6246a09427a0f86299ec
abdullaku <abdulla.ali.kurd@gmail.com>
2026-03-15 01:31:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN", "8611761761:AAEU_XJjV8QQ3LPr2rWf6gDBNnH2TVbs3_E")
-    CHANNEL_ID = os.getenv("CHANNEL_ID", "@Economic_news_Kurdish")
+    CHANNEL_ID = os.getenv("CHANNEL_ID", "-1003829360084")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyDiLR-mTpcgqPyWgry2eZM9AJCoXODLDao")
     CHECK_INTERVAL_SECONDS = 1800
     POST_DELAY_SECONDS = 5
