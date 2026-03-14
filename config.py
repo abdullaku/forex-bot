63356e934ece3d10c0d2753b68dbd81c15fa0ab6
abdullaku <abdulla.ali.kurd@gmail.com>
2026-03-14 23:14:07 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8611761761:AAEU_XJjV8QQ3LPr2rWf6gDBNnH2TVbs3_E")
+    CHANNEL_ID = os.getenv("CHANNEL_ID", "@Economic_news_Kurdish")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "YOUR_GEMINI_KEY_HERE")
+    CHECK_INTERVAL_SECONDS = 1800
+    POST_DELAY_SECONDS = 5
+    MAX_POSTS_PER_DAY = 20
