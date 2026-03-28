3c4f651cd4292ba49d229cb97d14179b41f8e2e3
AbdullaBotani <abdulla.ali.kurd@gmail.com>
2026-03-28 18:06:33 +0300

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import timezone, timedelta
 
 class Config:
     TOKEN = os.environ.get("TELEGRAM_TOKEN")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 0))
     FACEBOOK_PAGE_TOKEN = os.environ.get("FACEBOOK_PAGE_TOKEN")
     FACEBOOK_PAGE_ID = os.environ.get("FACEBOOK_PAGE_ID")
 
