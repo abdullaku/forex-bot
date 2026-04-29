a189557f3b851a76688e2711794784230d4c8d31
AbdullaBotani <abdulla.ali.kurd@gmail.com>
2026-04-29 23:01:53 +0300

    config.py

--- config.py
+++ config.py
@@ -17,13 +17,11 @@ class Config:
 
     DINAR_API_TOKEN = os.environ.get("DINAR_API_TOKEN", "")
 
-    # Economic Calendar
-    CALENDAR_PROVIDER = os.environ.get("CALENDAR_PROVIDER", "tradingeconomics")
-    TRADING_ECONOMICS_API_KEY = os.environ.get("TRADING_ECONOMICS_API_KEY", "")
-    TRADING_ECONOMICS_COUNTRIES = os.environ.get("TRADING_ECONOMICS_COUNTRIES", "All")
+    # Economic Calendar - ForexFactory only
+    CALENDAR_PROVIDER = os.environ.get("CALENDAR_PROVIDER", "forexfactory")
     DAILY_CALENDAR_FETCH_TIME = os.environ.get("DAILY_CALENDAR_FETCH_TIME", "08:55")
     DAILY_CALENDAR_POST_TIME = os.environ.get("DAILY_CALENDAR_POST_TIME", "09:00")
     PRE_ALERT_MINUTES = int(os.environ.get("PRE_ALERT_MINUTES", "30"))
     RESULT_POLL_SECONDS = int(os.environ.get("RESULT_POLL_SECONDS", "60"))
     RESULT_POLL_WINDOW_MINUTES = int(os.environ.get("RESULT_POLL_WINDOW_MINUTES", "15"))
-    FOREX_FACTORY_FALLBACK = os.environ.get("FOREX_FACTORY_FALLBACK", "true")
+    CALENDAR_REFRESH_NORMAL_MINUTES = int(os.environ.get("CALENDAR_REFRESH_NORMAL_MINUTES", "360"))
